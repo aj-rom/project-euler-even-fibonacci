767f4cf8f5d51dad6db44299769b607115f2da8e
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-09 00:14:50 -0700

    Completed.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(num)
+  arr = []
+  i, first, second = 0, 0, 1
+  while i < num
+    i = first + second
+    break if i > num
+
+    arr << i if i.even?
+    first = second
+    second = i
+  end
+  arr.sum
+end
+
+x = even_fibonacci_sum 100
+
+puts "#{x}"
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+  attr_accessor :limit
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+      arr = []
+      i, first, second = 0, 0, 1
+      while i < @limit
+        i = first + second
+        break if i > @limit
+
+        arr << i if i.even?
+        first = second
+        second = i
+      end
+      arr.sum
+  end
+end
